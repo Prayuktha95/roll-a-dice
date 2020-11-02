29d42df4bebc82bf905f337a01469a07436727f8
Prayuktha95 <56911814+Prayuktha95@users.noreply.github.com>
2020-11-02 12:10:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-import random
-def roll_a_dice():
-	return print(random.randint(1,6))
-
-roll_a_dice()
+import random 
+answer = random.randint(1,6)
+while True:
+	try:
+		guess = int(input('Guess a number 1~6:  '))
+		if 0 < guess < 7:
+			if answer == guess:
+				print('you guessed right!')
+				break
+			else:
+				print('guess again')
+		else:
+			print('the number must be between 1 and 6')
+	except:
+		print('type a number')
